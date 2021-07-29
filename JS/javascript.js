52ce7c898554be662403186a0f3932dfd1a17a1f
Amin Bitaraf <aminlbitaraf@gmail.com>
2021-07-29 18:15:06 +0430

    add transition for part1 & part2

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,9 +1,8 @@
-var part1 = '<div id="part1" class="container , noselect">' + '<div id="dipHead" class="noselect active">' + '<h1>Dipper</h1>' + '</div>' + '<div id="wellcomeMsg" class="noselect">' + '<p>Hi, my name is Mahdi.</br>Wellcome to my webpage!</br>I see this webpage as my resume</br>contact me if you are intrested :)</p>' + '</div>' + '</div>'
-var part2 = '<div id="part2" class="container , noselect">' + '<div id="amHead">' + '<h2>About me</h2>' + '</div>' + '<div id="amPara" class="noselect">' + '<p>My full name is Mahdi Kafash Kohan.</br>I am fluent in HTML and CSS, </br> also I am fimiliar with JS, JQuery, SASS, Bootstrap, Python and C++. </br> <strong> I learned self - Tutorial </strong>. </p> ' + ' </div>' + '</div>'
+var part1 = '<div id="part1" class="container noselect">' + '<div id="dipHead" class="noselect">' + '<h1>Dipper</h1>' + '</div>' + '<div id="wellcomeMsg" class="noselect">' + '<p>Hi, my name is Mahdi.</br>Wellcome to my webpage!</br>I see this webpage as my resume</br>contact me if you are intrested :)</p>' + '</div>' + '</div>'
+var part2 = '<div id="part2" class="container noselect">' + '<div id="amHead">' + '<h2>About me</h2>' + '</div>' + '<div id="amPara" class="noselect">' + '<p>My full name is Mahdi Kafash Kohan.</br>I am fluent in HTML and CSS, </br> also I am fimiliar with JS, JQuery, SASS, Bootstrap, Python and C++. </br> <strong> I learned self - Tutorial </strong>. </p> ' + ' </div>' + '</div>'
 var part3 = '<div id="part3">' + '<a id="perePJBtn" target="_blank"><div><img src="./SVG/prebtn.svg"></div></a>' + '<div id="slider" class="container , noselect">' + '<div id="image"><div ><img id="X" src="./images/1.png"></div></div>' + '<div id="commentHead"><h2>' + 'Multiplication table' + '</h2></div>' + '<div id="commentPara"><p>' + 'in this project you have to choose the right answer to the multiplication question. </br> please open with desktop browser.' + '</p></div>' + '<a id="commentGithub" href="' + 'https://github.com/DipperTheViper/Multiplication-Table' + '" target=”_blank”><div><img src="./SVG/github.svg"><span id="git">GitHub</span></div></a>' + '<a id="pjLink" href="' + './websites/1/index.html' + '" target=”_blank”><div><span id="pre">Preview</span><img id="pointer" src="./SVG/poinetr.svg"></div></a>' + '</div>' + '<a id="nextPJBtn" target="_blank"><div><img src="./SVG/nextbtn.svg"></div></a>' + '</div>'
 
-puttpart(part1);
-deactive("home");
+homePage();
 
 function deactive(id) {
     document.getElementById("home").style.setProperty("border-bottom", "none");
@@ -35,31 +34,33 @@ function scrolpos(val) {
 }
 
 
-document.getElementById("home").onclick = function() {
+function homePage() {
     puttpart(part1);
     deactive("home");
     scrolpos(1);
-    return false;
-};
+    setTimeout(function (){
+        document.getElementById("part1").classList.add('active')
+    },200);
+}
 
-document.getElementById("aboutMe").onclick = function() {
-    document.querySelector(".active").classList.remove("active");
+function aboutMe() {
     puttpart(part2);
-    document.querySelector("#amHead").classList.add("active");
     deactive("aboutMe");
     scrolpos(2);
-    return false;
-};
+    setTimeout(function (){
+        document.getElementById("part2").classList.add('active')
+    },100);
+}
 
-document.getElementById("myPJ").onclick = function() {
+function myProjects() {
     var slidenum = 0;
     let a = [{
-            title: '<h2>Multiplication table</h2>',
-            body: '<p>in this project you have to choose the right answer to the multiplication question. </br> please open with desktop browser.</p>',
-            linkgit: 'https://github.com/DipperTheViper/Multiplication-Table',
-            preview: './websites/1/index.html',
-            img: './images/1.png'
-        },
+        title: '<h2>Multiplication table</h2>',
+        body: '<p>in this project you have to choose the right answer to the multiplication question. </br> please open with desktop browser.</p>',
+        linkgit: 'https://github.com/DipperTheViper/Multiplication-Table',
+        preview: './websites/1/index.html',
+        img: './images/1.png'
+    },
         {
             title: '<h2>Cut the fruit</h2>',
             body: '<p>in this project you have to move the mouse under the fruit to cut them. </br> please open with desktop browser.</p>',
@@ -70,6 +71,9 @@ document.getElementById("myPJ").onclick = function() {
     ];
     puttpart(part3);
     deactive("myPJ");
+    setTimeout(function (){
+        document.getElementById("part3").classList.add('active')
+    },500);
     document.getElementById("nextPJBtn").onclick = function() {
         if (slidenum == 0) {
             slidenum++;
@@ -83,5 +87,4 @@ document.getElementById("myPJ").onclick = function() {
         }
     };
     scrolpos(3);
-    return false;
-};
+}
